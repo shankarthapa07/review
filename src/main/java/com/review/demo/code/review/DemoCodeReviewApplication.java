f952a9b2657eac552b0c989c1840982b7d50dabe
shankarthapa <sthapa04@gmail.com>
2026-05-26 03:22:25 +0530

    testing

---
+++ src/main/java/com/review/demo/code/review/DemoCodeReviewApplication.java
@@ -0,0 +1,13 @@
+package com.review.demo.code.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCodeReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCodeReviewApplication.class, args);
+	}
+
+}
